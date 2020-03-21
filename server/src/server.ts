eff944b5b64f1055071f0d129ff2705aecf034b6
Promie Yutasane <promie.yutasane@gmail.com>
2020-03-21 14:39:51 +1100

    Added more dependencies - cors and morgan logger

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
 import express, { Application, Request, Response, NextFunction } from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
 
 const app: Application = express()
 
 // Middlewares
+app.use(morgan('dev'))
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
